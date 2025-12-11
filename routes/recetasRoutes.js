1ec2e2f24a3865b5192dc2876e34889305365089
casa <andyjan.24@campuscamara.es>
2025-12-11 14:41:45 +0100

    servicios de backend completos menos user

--- routes/recetasRoutes.js
+++ routes/recetasRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const recetasController = require('../controllers/recetasController');
+// validador
+const { validatorCrearReceta } = require('../validators/recetaValidator');
 
 router.get('/', recetasController.obtenerTodos);
 router.get('/:id', recetasController.obtenerPorId);
-router.post('/', recetasController.crear);
+
+// inyectamos el middleware antes de llamar al controlador
+router.post('/', validatorCrearReceta, recetasController.crear);
+
 router.put('/:id', recetasController.actualizar);
 router.delete('/:id', recetasController.eliminar);
 
