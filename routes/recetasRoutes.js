a52f4f351068f63b69db9f906544e7ddc995923e
fran gonzalez <franciscomejias.24@campuscamara.es>
2025-12-11 16:38:55 +0100

    FEAT: Obtener mas valoradas, Obtener mas nuevas

--- routes/recetasRoutes.js
+++ routes/recetasRoutes.js
@@ -8,13 +8,15 @@ const { validatorCrearReceta } = require('../validators/recetaValidator');
 router.get('/', recetasController.obtenerTodos);
 router.get('/:id', recetasController.obtenerPorId);
 
+router.get('/mas-valoradas', recetasController.obtenerMasValoradas);
+router.get('/mas-nuevas', recetasController.obtenerMasNuevas);
+
 // inyectamos el middleware antes de llamar al controlador
 router.post('/', upload.array('imagenes', 5), validatorCrearReceta, recetasController.crear);
 router.post('/:id/valorar', recetasController.valorar);
 
 router.put('/:id', upload.array('imagenes', 5), recetasController.actualizar);
 router.delete('/:id', recetasController.eliminar);
-
 
 
 module.exports = router;
